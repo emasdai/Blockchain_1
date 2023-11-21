4f3aba24eb1ef4ab75d31eba49ccd6384c8068a9
emas dai <dikifarista61@gmail.com>
2023-11-21 08:10:11 +0000

    running and deploy storage

--- foundry-23/foundry-simple-storage-f23/script/DeploySimpleStorage.s.sol
+++ foundry-23/foundry-simple-storage-f23/script/DeploySimpleStorage.s.sol
@@ -1,12 +1,17 @@
 
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity 0.8.18;
 
-import {Script} from "forge-std/Script.sol";
-import {SimpleStorage} from ".../src/SimpleStorage.sol"
+pragma solidity 0.8.19;
+
+import {Script} from "foundry-23/foundry-simple-storage-f23/lib/forge-std/src/Script.sol";
+import {SimpleStorage} from "foundry-23/foundry-simple-storage-f23/src/SimpleStorage.sol";
 
 contract DeploySimpleStorage is Script{
-    function run()  external returns (SimpleStorage) {
-        vm.startBroadcast();   
+    function run() external returns (SimpleStorage){
+        vm.startBroadcast();    // memanggil function startbroadcast dari vm (foundry)
+        SimpleStorage simplestorage = new SimpleStorage();  // membuat storage baru
+        vm.stopBroadcast();
+        return simplestorage;
     }
 }
+
