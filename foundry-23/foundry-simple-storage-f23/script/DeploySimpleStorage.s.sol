5e11b2481832d962b9a6ba511e9b81da0e1d9f0f
emas dai <dikifarista61@gmail.com>
2023-11-21 06:24:14 +0000

    deploysimplestorage

---
+++ foundry-23/foundry-simple-storage-f23/script/DeploySimpleStorage.s.sol
@@ -0,0 +1,10 @@
+
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {SimpleStorage} from ".../src/SimpleStorage.sol"
+
+contract DeploySimpleStorage is Script{
+    
+}
