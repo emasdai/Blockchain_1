babb485f45a3f8247d8d3245d5707c4cbe9afa17
emas dai <dikifarista61@gmail.com>
2023-11-28 06:33:38 +0000

    add MockV3Aggregator.sol

--- foundry-23/foundry-fund-me-f23/script/HelperConfig.s.sol
+++ foundry-23/foundry-fund-me-f23/script/HelperConfig.s.sol
@@ -2,12 +2,15 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "../lib/forge-std/src/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
 
-contract HelperConfig {
+contract HelperConfig is Script{
     // jika kita berada di local chain seperti anvil, kita akan deploy mockup
     // mendapatkan address yang suda ada dari network
 
     NetworkConfig public activeNetworkConfig;   // mengatur network yang akan digunakan
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
 
     struct NetworkConfig {
         //   grouping together related data
@@ -18,7 +21,7 @@ contract HelperConfig {
         if (block.chainid == 1155111) {
             activeNetworkConfig = getSepoliaEthConfig();
         } else {
-            activeNetworkConfig = getAnvilEthConfig();
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
         }
     }
 
@@ -29,8 +32,22 @@ contract HelperConfig {
             return sepoliaConfig;
     } 
 
-    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
 
+        if(activeNetworkConfig.priceFeed != address(0)){    //default value
+            return activeNetworkConfig; // jika kita sudah membuat active network, maka akan kembali ke active network yang digunakan
+            
+        }
+        
+        // membuat active network jika belum pernah dibuat sebelumnya
+        vm.startBroadcast();   // menggunakan anvil saat melakukan config
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(DECIMALS, INITIAL_PRICE);
+        vm.stopBroadcast(); 
+
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            priceFeed: address(mockPriceFeed)
+        });
+        return anvilConfig;
     }
 
 }
